86126eead0cbe687d1e06345ac5644a284c3205e
ananyathakre <ananya.thakre@go-yubi.com>
2024-04-04 17:04:02 +0530

    error handled

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,70 +1,74 @@
 class EmployeesController < ApplicationController
-    skip_before_action :verify_authenticity_token
+	skip_before_action :verify_authenticity_token
+
 	def index
-		@employee = Employee.all
-		#render json: @employee	#not able to achieve filtering for this
-	end
-	def show
-		user = Employee.find_by(id: params[:user_id])
-		Current.current_user = user
-		authorize user
-		if policy(user).show?
-			if user.admin_status
-				@employees = Employee.all
-				render json: @employees
-			else
-				render json: { message: 'Employee details retrieved successfully', employee: user }
-			end
-		else
-			render json: { message: 'Unauthorized' }, status: :unauthorized
-		end
+	  @employees = Employee.all
+	  render json: @employees
 	end
 
-   def update
-		employee = Employee.find_by(id: params[:employee_id])
-		employee.update(emp_status: "terminated")
+	def show
+	  user = Employee.find_by(id: params[:user_id])
+	  Current.current_user = user
+	  if user.nil?
+		render json: { message: 'No employee found with the given ID' }, status: :not_found
+	  else
+		authorize user
+		if policy(user).show?
+		  if user.admin_status
+			@employees = Employee.all
+			render json: @employees
+		  else
+			render json: { message: 'Employee details retrieved successfully', employee: user }
+		  end
+		else
+		  render json: { message: 'Unauthorized' }, status: :unauthorized
+		end
+	  end
+	end
+
+	def update
+	  employee = Employee.find_by(id: params[:employee_id])
+	  if employee
+		employee.update(emp_status: 'terminated')
 		render json: employee
+	  else
+		render json: { message: 'Employee not found' }, status: :not_found
+	  end
 	end
 
 	def create
-		employee = Employee.new(employee_params(params))
-        Rails.logger.info "#{employee_params(params)}"
-		email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-		email_to_check = employee.email
-		if email_to_check =~ email_regex
-			if employee.valid?
-				employee.save!
-				render json: employee
-			else
-				render json: { details: employee.errors.full_messages }
-			end
-		else
-			render json: "Email pattern is incorrect!"
-		end
+	  employee = Employee.new(employee_params)
+	  if employee.valid?
+		employee.save!
+		render json: employee
+	  else
+		render json: { details: employee.errors.full_messages }, status: :unprocessable_entity
+	  end
 	end
 
 	def destroy
-		user = Employee.find_by(id: params[:user_id])
-		Current.current_user = user
-        authorize user
-		employee = Employee.find_by(id: params[:employee_id])
-		if user && employee
-		  if user.id == employee.id
-			render json: "Error"
-		  else
-			if employee.destroy
-			  render json: "Deletion done!"
-			else
-			  render json: "Error!"
-			end
-		  end
+	  user = Employee.find_by(id: params[:user_id])
+	  Current.current_user = user
+	  authorize user
+	  employee = Employee.find_by(id: params[:employee_id])
+	  if user && employee
+		if user.id == employee.id
+		  render json: 'Cannot delete yourself', status: :unprocessable_entity
 		else
-		  render json: "User or employee not present!"
+		  if employee.destroy
+			render json: 'Deletion done!'
+		  else
+			render json: 'Error deleting employee', status: :unprocessable_entity
+		  end
 		end
+	  else
+		render json: 'Employee not present', status: :not_found
+	  end
 	end
 
 	private
-	def employee_params(params)
-		params.permit(%w[name email department admin_status emp_status])
+
+	def employee_params
+	  params.require(:employee).permit(:name, :email, :department, :admin_status, :emp_status)
 	end
-end
+  end
